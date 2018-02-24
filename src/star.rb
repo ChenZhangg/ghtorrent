aa31b0f15781f40978c55ccd3ddc8cce1f828b10
zc <340355960@qq.com>
2018-02-24 10:21:54 +0800

    10:21

--- src/star.rb
+++ src/star.rb
@@ -3,26 +3,9 @@ require 'open-uri'
 require 'smarter_csv'
 require 'travis'
 require 'mysql2'
-@account=['528447050@139.com',
-'15005210142@sohu.com',
-'chenzhangfdse@sina.com',
-'chenzhangfdse@outlook.com',
-'chenzhangfdse@21cn.com',
-'mfkzihys@dongqing365.com',
-'erqrer@dongqing365.com',
-'erqrer2fs@dongqing365.com',
-'thyjyue@dongqing365.com',
-'thyjyueee@dongqing365.com',
-'etregggg@dongqing365.com',
-'rgnggs@dongqing365.com',
-'yulg@dongqing365.com',
-'iiilds@dongqing365.com',
-'zchnd@dongqing365.com',
-'yukjg@dongqing365.com',
-'ukld@dongqing365.com',
-'lopf@dongqing365.com',
-'opgs@dongqing365.com',
-'plh@dongqing365.com'
+@account=['shunervw497',
+'louqianzyq220808@163.com',
+'340355960@qq.com'
 ]
 @count=0
 def changeAccount(i)
@@ -36,15 +19,14 @@ def getProjectStar(repo_name)
     f=open(url,:http_basic_authentication=>[@account[@count], 'cumtzc04091751'])
     stars=JSON.parse(f.read)['stargazers_count']
     puts "#{repo_name}     #{stars}"
-    puts "#{@account[@count]}     #{f.meta["x-ratelimit-remaining"]}"
-    @count+=1 if f.meta["x-ratelimit-remaining"].to_i<30
-
+    puts "#{repo_name}     #{@account[@count]}     #{f.meta["x-ratelimit-remaining"]}"
+    @count+=1 if f.meta["x-ratelimit-remaining"].to_i<10
   rescue => e
-    puts "===#{e.message}"
+    puts "#{e.message}"
     k+=1
     stars=0
-    #sleep 10
-    #retry if k<3
+    sleep 10
+    retry if k<3 && !(e.message.include?('404'))
   end
   stars
 end
@@ -61,6 +43,7 @@ def getTravisBuildNumber(repo_name)
     builds=0
   end
   puts "#{repo_name}     #{builds}"
+  builds
 end
 
 def mysql_initiallize
@@ -68,6 +51,7 @@ def mysql_initiallize
   results = @client.query('CREATE DATABASE IF NOT EXISTS zc')
   results = @client.query('USE zc')
   results = @client.query('CREATE TABLE IF NOT EXISTS repository(
+    id    int,
     reponame varchar(255),
     stars int,
     builds int
@@ -76,14 +60,16 @@ end
 
 def scanCSV(csv_file_path)
   mysql_initiallize
+  count=0
   SmarterCSV.process(csv_file_path, {:chunk_size => 10, :headers_in_file => false, :user_provided_headers => [:url, :repo_name]}) do |chunk|
     chunk.each do |row|
       stars=getProjectStar(row[:repo_name])
       builds=getTravisBuildNumber(row[:repo_name])
-      statement = @client.prepare('INSERT INTO repository(reponame,stars,builds) VALUES(?,?,?);')
-      statement.execute(row[:repo_name],stars,builds)
+      puts
+      statement = @client.prepare('INSERT INTO repository(id,reponame,stars,builds) VALUES(?,?,?,?);')
+      statement.execute(count,row[:repo_name],stars,builds)
+      count+=1
     end
   end
 end
-scanCSV('test.csv')
-#getProjectStar('rails/rails')
+scanCSV('../data/test.csv')
