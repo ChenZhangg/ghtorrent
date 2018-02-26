553e401edd55897a6309d9c23a75b2c3bf3ac631
zc <340355960@qq.com>
2018-02-26 15:11:46 +0800

    2:55

--- src/run.sh
+++ src/run.sh
@@ -13,8 +13,8 @@ do
     done
 done < $file
 
-for((i=0;i<1;i=i+1))
+for((i=0;i<28;i=i+1))
 do
-  (ruby star.rb ../data/repo$i ${account[$i]} ${password[$i]})
-  #(nohup ruby star.rb ../data/repo$i ${account[$i]} ${password[$i]}> output$i 2>&1 & )&
+  #(ruby star.rb ../data/repo$i ${account[$i]} ${password[$i]})
+  (nohup ruby star.rb ../data/repo$i ${account[$i]} ${password[$i]}> output$i 2>&1 & )&
 done
